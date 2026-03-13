7c46f4959d7a96fa0601d84b219c6c73e18a0f2e
Kreestoff2024 <christophe.lefrancois2024@campus-eni.fr>
2026-03-13 16:19:55 +0100

    added python file for browser options

---
+++ Resources/browser_options.py
@@ -0,0 +1,8 @@
+from selenium.webdriver.chrome.options import Options
+
+def get_chrome_options():
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    return options
